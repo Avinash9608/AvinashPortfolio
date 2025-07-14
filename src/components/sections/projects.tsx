4925dbc8bc325e11acbad7b642ee53d1955b4107
Manorama <m3361555@gmail.com>
2025-07-14 19:36:26 +0000

    https://studio--blogger-showcase.us-central1.hosted.app/
    Blogging Platf

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -41,7 +41,7 @@ const projects = [
     image: 'https://plus.unsplash.com/premium_photo-1720744786849-a7412d24ffbf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmxvZ3xlbnwwfHwwfHx8MA%3D%3D',
     tags: ['Full-Stack', 'MongoDB', 'CMS'],
     github: '#',
-    live: '#',
+    live: 'https://studio--blogger-showcase.us-central1.hosted.app/',
     aiHint: 'blog website'
   },
   {
